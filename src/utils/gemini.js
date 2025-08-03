9e80a78684f92b98c80ad17042ef75a123eb7331
fazle-rabbi-dev <frabby983@gmail.com>
2025-08-03 10:28:02 +0600

    :tada Change model name

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -11,7 +11,7 @@ export async function getSummaryFromGemini(prompt) {
 	const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 	const model = genAI.getGenerativeModel({
-		model: "gemini-2.5-flash-preview-04-17",
+		model: "gemini-2.5-flash",
 	});
 	const result = await model.generateContent(prompt);
 	const response = await result.response;
